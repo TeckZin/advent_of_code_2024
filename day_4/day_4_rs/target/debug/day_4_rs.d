b2848efba45c000e5ebd4bbc008e3dcbba6ac44f
TeckWIT <tant1@wit.edu>
2024-12-12 11:49:59 +0800

    more changes

---
+++ day_4/day_4_rs/target/debug/day_4_rs.d
@@ -0,0 +1,1 @@
+/Users/teck/dev/personal/AdventOfCode/advent_of_code_2024/day_4/day_4_rs/target/debug/day_4_rs: /Users/teck/dev/personal/AdventOfCode/advent_of_code_2024/day_4/day_4_rs/src/main.rs
---
+++ day_4/day_4_rs/target/debug/deps/day_4_rs-82dc8a5a2514c005.d
@@ -0,0 +1,5 @@
+/Users/teck/dev/personal/AdventOfCode/advent_of_code_2024/day_4/day_4_rs/target/debug/deps/libday_4_rs-82dc8a5a2514c005.rmeta: src/main.rs
+
+/Users/teck/dev/personal/AdventOfCode/advent_of_code_2024/day_4/day_4_rs/target/debug/deps/day_4_rs-82dc8a5a2514c005.d: src/main.rs
+
+src/main.rs:
