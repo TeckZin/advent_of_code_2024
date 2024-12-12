d80169710fdbfa466cf74fe191e1f05bf4869d17
TeckWIT <tant1@wit.edu>
2024-12-12 10:28:54 +0800

    more rust lmao

--- day_4/day_4_rs/src/main.rs
+++ day_4/day_4_rs/src/main.rs
@@ -17,12 +17,48 @@ fn read_file() -> Result<String, std::io::Error> {
 
 fn part_1(content: String) {
     println!("{}", content);
+
+    // dia
+
     let content_rows: Vec<&str> = content.lines().collect();
     // also works
     // println!("{:?}", content_rows);
     println!("{:#?}", content_rows);
+    let mut content_columns: Vec<String> = vec![String::new(); content_rows.len()];
+    for line in content.lines() {
+        for (i, c) in line.chars().enumerate() {
+            content_columns[i].push(c);
+        }
+    }
+
+    for col in &content_columns {
+        println!("col {}", col);
+        for (index, _) in col.match_indices("X") {
+            if index + 4 < col.len() {
+                let slice = &col[index..index + 4];
+                if slice == "XMAS" {
+                    println!("{}", slice);
+                    println!("next\n");
+                }
+            }
+        }
+
+        let rev_col: String = col.chars().rev().collect();
+
+        for (index, _) in rev_col.match_indices("X") {
+            if index + 4 < col.len() {
+                let slice = &rev_col[index..index + 4];
+                if slice == "XMAS" {
+                    println!("{}", slice);
+                    println!("next\n");
+                }
+            }
+        }
+    }
+
+    // println!("{:#?}", content_columns);
     for row in &content_rows {
-        println!("row {}", row);
+        // println!("row {}", row);
 
         // let index: i32 = match row.find('X') {
         //     Some(index) => index as i32,
@@ -41,12 +77,30 @@ fn part_1(content: String) {
         //     let slice = &row[index..index + 4];
         //     println!("{}", slice);
         // }
+        //
+        //
+
+        // HORIZONTAL
 
         for (index, _) in row.match_indices("X") {
-            let slice = &row[index..index + 4];
-            if slice == "XMAS" {
-                println!("{}", slice);
-                println!("next\n");
+            if index + 4 < row.len() {
+                let slice = &row[index..index + 4];
+                if slice == "XMAS" {
+                    // println!("{}", slice);
+                    // println!("next\n");
+                }
+            }
+        }
+
+        let rev_row: String = row.chars().rev().collect();
+
+        for (index, _) in rev_row.match_indices("X") {
+            if index + 4 < row.len() {
+                let slice = &rev_row[index..index + 4];
+                if slice == "XMAS" {
+                    // println!("{}", slice);
+                    // println!("next\n");
+                }
             }
         }
     }
---
+++ day_4/day_4_rs/target/debug/deps/day_4_rs-99211afc754f69f4.d
@@ -0,0 +1,5 @@
+/Users/teck/dev/personal/AdventOfCode/advent_of_code_2024/day_4/day_4_rs/target/debug/deps/day_4_rs-99211afc754f69f4: src/main.rs
+
+/Users/teck/dev/personal/AdventOfCode/advent_of_code_2024/day_4/day_4_rs/target/debug/deps/day_4_rs-99211afc754f69f4.d: src/main.rs
+
+src/main.rs:
